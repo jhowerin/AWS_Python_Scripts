0dc7490a580e21a68b5eff267dfce16f80983ebb
Jake Howering <jhowerin@gmail.com>
2022-09-26 12:57:22 -0400

    Minor updates

--- aws_get_VM_Name_IP_FQDN.py
+++ aws_get_VM_Name_IP_FQDN.py
@@ -3,8 +3,8 @@
 # 1. AWS CLI installed and configured
 # 2. Python 3.6 or higher
 # 3. Boto3 installed
-# 4. You must your AWS Master/Main Account ID
-# 5. You must be able to assume role into your AWS Sub Accounts
+# 4. You must know your AWS Master/Main Account ID
+# 5. You must be able to assume role into your AWS Sub Accounts from the Master/Main Account
 import boto3
 import json
 orgClient = boto3.client('organizations')
@@ -13,16 +13,23 @@ response = orgClient.list_accounts()
 # comment this out when you are done analyzing the response
 # print(json.dumps(response, indent=4, sort_keys=True, default=str))
 # print(response)
+print("******************************************************************")
 print("Welcome to the AWS Organization EC2 Name, IP and FQDN Report")
 print("To get started, please enter the AWS Organization Main Account ID: ")
 orgMainAccountID = input()
 print("*********************************")
+# Print the list of accounts in the Organization
+print('The Accounts in the Organization:')
+for account in response['Accounts']:
+    print(account['Name'])
+print("*********************************")
+print("All the VMs in all the Accounts in the Organization:")
 for account in response['Accounts']:
     if account["Id"] == orgMainAccountID:
             # print("Checking the AWS Organization Master Account for the EC2 resources")
-            print(account['Name'], account['Id'], account['Email'], account['Status'], "Master Account")
-            # Access EKS resources in all regions
-            # The ec2 client is used to get the list of all regions
+            print("  ", account['Name'], account['Id'], account['Email'], account['Status'], "Master Account")
+            # Access EC@ resources in all regions
+            # The ec2 client is used to get the list of all regions in the AWS account
             ec2_client = boto3.client('ec2')
             response = ec2_client.describe_regions()
             # Loop through all regions
@@ -37,23 +44,25 @@ for account in response['Accounts']:
                         # Only print running instances
                         try:
                             if instance["State"]["Name"] == "running":
-                                print("Name: {}, InstanceId: {}, IP: {}, FQDN: {}".format(
+                                print("    ", "Region: {}, Name: {}, Private IP: {}, Public IP: {}, FQDN: {}".format(
+                                    region['RegionName'],
                                     # get instance name from Tag Name
                                     [tag['Value'] for tag in instance['Tags'] if tag['Key'] == 'Name'][0],
-                                    instance["InstanceId"],
                                     instance["PrivateIpAddress"],
-                                    instance["PrivateDnsName"]
+                                    instance["PublicIpAddress"],
+                                    #instance["PrivateDnsName"]
+                                    instance["PublicDnsName"]
                                 ))
                         except KeyError as missing_key:
                             # Used as missing_key for readability purposes only
                             print(f"Trying to access a <dict> with a missing key {missing_key}")
-            print("*********************************")
+            print("*****")
     else:       
         # Check only "Active" accounts
         if account['Status'] == 'ACTIVE':
             # Assume role into child account
             if account["Id"] != orgMainAccountID:
-                print(account['Name'], account['Id'], account['Email'], account['Status'])
+                print("  ", account['Name'], account['Id'], account['Email'], account['Status'])
                 stsClient = boto3.client('sts')
                 roleArn = "arn:aws:iam::" + account['Id'] + ":role/OrganizationAccountAccessRole"
                 stsresponse = stsClient.assume_role(RoleArn=roleArn, RoleSessionName='newsession')
@@ -80,14 +89,18 @@ for account in response['Accounts']:
                             # Only print running instances
                             try:
                                 if instance["State"]["Name"] == "running":
-                                    print("Name: {}, InstanceId: {}, IP: {}, FQDN: {}".format(
+                                    print("    ", "Region: {}, Name: {}, Private IP: {}, Public IP: {}, FQDN: {}".format(
+                                        region['RegionName'],
                                         # get instance name from Tag Name
                                         [tag['Value'] for tag in instance['Tags'] if tag['Key'] == 'Name'][0],
                                         instance["InstanceId"],
                                         instance["PrivateIpAddress"],
-                                        instance["PrivateDnsName"]
+                                        instance["PublicIpAddress"],
+                                        #instance["PrivateDnsName"]
+                                        instance["PublicDnsName"]
                                     ))
                             except KeyError as missing_key:
                                 # Used as missing_key for readability purposes only
                                 print(f"Trying to access a <dict> with a missing key {missing_key}")
-            print("*********************************")
+            print("*****")
+print("******************************************************************")
