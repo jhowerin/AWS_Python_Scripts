d814bd6ff7829e59305c2faf98fb305e6d93dda7
Jake Howering <jhowerin@gmail.com>
2022-10-04 08:24:04 -0400

    File Structure Update

--- VM_IP_FQDN/VM_NAME_IP_FQDN.py
+++ VM_IP_FQDN/VM_NAME_IP_FQDN.py
@@ -1,3 +1,6 @@
+# Jake Howering
+# jhowerin@gmail.com
+# Oct 2022
 # Get all AWS EC2 Names, IPs and FQDNs
 # Requirements to run script
 # 1. AWS CLI installed and configured
@@ -7,62 +10,57 @@
 # 5. You must be able to assume role into your AWS Sub Accounts from the Master/Main Account
 import boto3
 import json
-orgClient = boto3.client('organizations')
-response = orgClient.list_accounts()
-# json dump to see the structure of the response
-# comment this out when you are done analyzing the response
-# print(json.dumps(response, indent=4, sort_keys=True, default=str))
-# print(response)
+
 print("******************************************************************")
 print("Welcome to the AWS Organization EC2 Name, IP and FQDN Report")
 print("To get started, please enter the AWS Organization Main Account ID: ")
+orgClient = boto3.client('organizations')
+response = orgClient.list_accounts()
 orgMainAccountID = input()
+# print out the json dump to find investigate the json response
+# print(json.dumps(response, indent=4, sort_keys=True, default=str))
 print("*********************************")
 # Print the list of accounts in the Organization
 print('The Accounts in the Organization:')
 for account in response['Accounts']:
-    print(account['Name'])
+    print(account['Name'] + ",", account['Id'] + ",", account['Email'])
 print("*********************************")
 print("All the VMs in all the Accounts in the Organization:")
+# Iterate through the list of accounts in the Organization from the response above
 for account in response['Accounts']:
     if account["Id"] == orgMainAccountID:
-            # print("Checking the AWS Organization Master Account for the EC2 resources")
-            print("  ", account['Name'], account['Id'], account['Email'], account['Status'], "Master Account")
-            # Access EC@ resources in all regions
-            # The ec2 client is used to get the list of all regions in the AWS account
-            ec2_client = boto3.client('ec2')
-            response = ec2_client.describe_regions()
-            # Loop through all regions
-            for region in response['Regions']:
-                ec2Client = boto3.client('ec2', region_name=region['RegionName'])
-                # print("AWS Main Account with region",region['RegionName'])
-                # Get all EC2 instances
-                response = ec2Client.describe_instances()
-                # Loop through all instances
-                for reservation in response["Reservations"]:
-                    for instance in reservation["Instances"]:
-                        # Only print running instances
-                        try:
-                            if instance["State"]["Name"] == "running":
-                                print("    ", "Region: {}, Name: {}, Private IP: {}, Public IP: {}, FQDN: {}".format(
-                                    region['RegionName'],
-                                    # get instance name from Tag Name
-                                    [tag['Value'] for tag in instance['Tags'] if tag['Key'] == 'Name'][0],
-                                    instance["PrivateIpAddress"],
-                                    instance["PublicIpAddress"],
-                                    #instance["PrivateDnsName"]
-                                    instance["PublicDnsName"]
-                                ))
-                        except KeyError as missing_key:
-                            # Used as missing_key for readability purposes only
-                            print(f"Trying to access a <dict> with a missing key {missing_key}")
-            print("*****")
+        # The ec2 client is used to get the list of all regions in the AWS account
+        ec2_client = boto3.client('ec2')
+        response = ec2_client.describe_regions()
+        # Loop through all regions
+        for region in response['Regions']:
+            ec2Client = boto3.client('ec2', region_name=region['RegionName'])
+            # Get all EC2 instances
+            response = ec2Client.describe_instances()
+            # Loop through all instances
+            for reservation in response["Reservations"]:
+                for instance in reservation["Instances"]:
+                    # Only print running instances
+                    try:
+                        if instance["State"]["Name"] == "running":
+                            print("Account Name:", account['Name']+",", "Region: {}, Name: {}, Private IP: {}, Public IP: {}, FQDN: {}".format(
+                                region['RegionName'],
+                                # get instance name from Tag Name
+                                [tag['Value'] for tag in instance['Tags'] if tag['Key'] == 'Name'][0],
+                                instance["PrivateIpAddress"],
+                                instance["PublicIpAddress"],
+                                #instance["PrivateDnsName"]
+                                instance["PublicDnsName"]
+                            ))
+                    except KeyError as missing_key:
+                        # Used as missing_key for readability purposes only
+                        print(f"Trying to access a <dict> with a missing key {missing_key}")
+        print("*****")
     else:       
         # Check only "Active" accounts
         if account['Status'] == 'ACTIVE':
             # Assume role into child account
             if account["Id"] != orgMainAccountID:
-                print("  ", account['Name'], account['Id'], account['Email'], account['Status'])
                 stsClient = boto3.client('sts')
                 roleArn = "arn:aws:iam::" + account['Id'] + ":role/OrganizationAccountAccessRole"
                 stsresponse = stsClient.assume_role(RoleArn=roleArn, RoleSessionName='newsession')
@@ -80,16 +78,16 @@ for account in response['Accounts']:
                         aws_access_key_id=newsession_id, 
                         aws_secret_access_key=newsession_key, 
                         aws_session_token=newsession_token)
-                    # print("AWS Sub Account", account['Name'], "with region",region['RegionName'])
                     # Get all EC2 instances
                     response = ec2Client.describe_instances()
                     # Loop through all instances
                     for reservation in response["Reservations"]:
                         for instance in reservation["Instances"]:
                             # Only print running instances
+                            # print(json.dumps(instance, indent=4, sort_keys=True, default=str))
                             try:
                                 if instance["State"]["Name"] == "running":
-                                    print("    ", "Region: {}, Name: {}, Private IP: {}, Public IP: {}, FQDN: {}".format(
+                                    print("Account Name:",account['Name']+",", "Region: {}, Name: {}, Private IP: {}, Public IP: {}, FQDN: {}".format(
                                         region['RegionName'],
                                         # get instance name from Tag Name
                                         [tag['Value'] for tag in instance['Tags'] if tag['Key'] == 'Name'][0],
@@ -101,6 +99,6 @@ for account in response['Accounts']:
                                     ))
                             except KeyError as missing_key:
                                 # Used as missing_key for readability purposes only
-                                print(f"Trying to access a <dict> with a missing key {missing_key}")
+                                print(f"Trying to access a <dict> with a missing key {missing_key}") 
             print("*****")
 print("******************************************************************")
